1dda8c1d8945b572ab5398150d62ebdee91526a5
Travis Xie <yinuox@uw.edu>
2021-02-01 16:31:49 -0800

    Add comments and clearn the code

--- Assignment 3/a3-starter/agents/backgammon_dsbg.py
+++ Assignment 3/a3-starter/agents/backgammon_dsbg.py
@@ -8,18 +8,18 @@ from game_engine import genmoves
 class BackgammonPlayer:
     def __init__(self):
         self.GenMoveInstance = genmoves.GenMoves()
-        self.GenMoveInstance = genmoves.GenMoves()
-        self.states = 0
-        self.cutoffs = 0
-        self.maxply = 3
+        self.states = 0              # number of states explored
+        self.cutoffs = 0             # number of states that is being cutoff
+        self.maxply = 3              # max depth
         self.MIN = -100000
         self.MAX = 100000
-        self.prune = True
-        self.dict = {}               # dictionary that keep tracks of the best value and its move
+        self.prune = True            # whether to use alpha-beta pruning 
+        self.dict = {}               # dictionary that keep tracks of the best value (key) and its move
 
     # returns a string representing a unique nick name for your agent
     def nickname(self):
         return "yinuox, xuanry"
+
 
     # If prune==True, changes the search algorthm from minimax
     # to Alpha-Beta Pruning
@@ -28,10 +28,12 @@ class BackgammonPlayer:
         self.cutoffs = 0
         self.prune = prune
 
+
     # Returns a tuple containing the number explored
     # states as well as the number of cutoffs.
     def statesAndCutoffsCounts(self):
         return (self.states, self.cutoffs)
+
 
     # Given a ply, it sets a maximum for how far an agent
     # should go down in the search tree. If maxply==-1,
@@ -40,19 +42,22 @@ class BackgammonPlayer:
         if maxply != -1:
             self.maxply = maxply
 
+
     # If not None, it update the internal static evaluation
     # function to be func
     def useSpecialStaticEval(self, func=None):
         if func is not None:
             self.staticEval = func
 
+
     def initialize_move_gen_for_state(self, state, who, die1, die2):
         return self.GenMoveInstance.gen_moves(state, who, die1, die2)
+
 
     # Given a state and a roll of dice, it returns the best move for
     # the state.whose_move
     def move(self, state, die1=1, die2=6):
-        self.dict = {}                  # reset the dictionary for every move
+        self.dict.clear()                # reset the dictionary for every new move
         value = self.minimax(state, state.whose_move, self.maxply, self.MIN, self.MAX, die1, die2)
         return self.dict[value]
 
@@ -61,7 +66,10 @@ class BackgammonPlayer:
     # for the two players (W and R) -- more positive numbers are good for W
     # while more negative numbers are good for R
     def staticEval(self, state):
-        red = 0
+        ''' Count the pips counts for white and red player. 
+        Fewer pips counts means better
+        '''
+        red = 0                    # pips counts 
         white = 0
         
         for i in range(24):
@@ -79,74 +87,68 @@ class BackgammonPlayer:
         
         red -= 25 * len(state.red_off)
         white -= 25 * len(state.white_off)
+
+        # return the difference
+        # more positive means good for white (white gets closer to win)
+        # negative means good for red (red gets closer to win)
         return red - white
+
 
     def minimax(self, state, whoseMove, plyLeft, alpha, beta, die1, die2):
         ''' minmax search. Search for the best move and its correspoding staticEval
         for the current state and the current player (whoseMove)
         return a paire of (moove, value)
         '''
-        if plyLeft == 0:
+        if plyLeft == 0:        
             return self.staticEval(state)
 
         if whoseMove == 0:  
             bestVal = self.MIN
-            bestMove = None
-            move_state_list = self.get_all_possible_moves_state(state, die1, die2, plyLeft)
-            non_cut_off = 0  
-            for (m, s) in move_state_list:
-                non_cut_off += 1
-                self.states += 1
-                newVal = self.minimax(s, s.whose_move, plyLeft - 1, alpha, beta, die1, die2)
-                if newVal > bestVal:                  
-                    bestVal = newVal
-                    bestMove = m 
-
-                if self.prune:
-                    alpha = max(alpha, bestVal)
-                    if alpha >= beta:
-                        self.cutoffs = self.cutoffs + len(move_state_list) - non_cut_off
-                        break 
-
-            self.dict[bestVal] = bestMove
-            return bestVal
         else:
             bestVal = self.MAX
-            move_state_list = self.get_all_possible_moves_state(state, die1, die2, plyLeft)
-            non_cut_off = 0  
-            for (m, s) in move_state_list:
-                non_cut_off += 1
-                self.states += 1
-                newVal = self.minimax(s, 1 - whoseMove, plyLeft - 1, alpha, beta, die1, die2)
-                if newVal < bestVal:                  
-                    bestVal = newVal
-                    bestMove = m
 
-                # using alpha beta pruning 
-                if self.prune:
+        bestMove = None
+        move_state_list = self.get_all_possible_moves_state(state, die1, die2)
+        non_cut_off = 0    # record the number of states that are not cut off
+        for (m, s) in move_state_list:
+            non_cut_off += 1      
+            self.states += 1
+            newVal = self.minimax(s, s.whose_move, plyLeft - 1, alpha, beta, die1, die2)
+            if (whoseMove == 0 and newVal > bestVal) or \
+                (whoseMove == 1 and newVal < bestVal):                  
+                bestVal = newVal
+                bestMove = m 
+
+            # alpha beta pruning 
+            if self.prune:
+                if whoseMove == 0:
+                    alpha = max(alpha, bestVal)
+                else:
                     beta = min(beta, bestVal)
-                    if alpha >= beta:
-                        self.cutoffs = self.cutoffs + len(move_state_list) - non_cut_off
-                        break
 
-            self.dict[bestVal] = bestMove
-            return bestVal
+                if alpha >= beta:
+                    self.cutoffs = self.cutoffs + len(move_state_list) - non_cut_off
+                    break 
 
-    def get_all_possible_moves_state(self, state, die1, die2, plyLeft):
+        self.dict[bestVal] = bestMove
+        return bestVal
+
+
+    def get_all_possible_moves_state(self, state, die1, die2):
         """Uses the mover to generate all legal (moves, state). Returns an array of tuples"""
+        # reset the move_generator to every new state passed in
         move_generator = self.initialize_move_gen_for_state(state, state.whose_move, die1, die2)
         move_state_list = []
         done_finding_moves = False
         any_non_pass_moves = False
         while not done_finding_moves:
             try:
-                m = next(move_generator)         # Gets a (move, state) pair.
-                #print("next returns: ", m[0], 'who: ', state.whose_move, 'plyLeft: ', plyLeft)          # Prints out the move.    For debugging.
+                m = next(move_generator)         # Gets a (move, state) pair.        
                 if m[0] != 'p':                          
                     any_non_pass_moves = True          
                     move_state_list.append(m)    # Add the (move, state) to the list.
             except StopIteration as e:
                 done_finding_moves = True
-        if not any_non_pass_moves:
-            move_state_list.append(('p', state))
+        if not any_non_pass_moves:                  # if no move, then the state doesn't change
+            move_state_list.append(('p', state))    
         return move_state_list
--- Assignment 3/a3-starter/agents/backgammon_ssbg.py
+++ Assignment 3/a3-starter/agents/backgammon_ssbg.py
@@ -9,19 +9,23 @@ from game_engine import genmoves
 class BackgammonPlayer:
     def __init__(self):
         self.GenMoveInstance = genmoves.GenMoves()
-        self.maxply = 2
+        self.maxply = 2      
         self.MIN = -100000
         self.MAX = 100000
         self.dict = {}               # dictionary that keep tracks of the best value and its move
-        self.dice = []
+        self.dice = []               # possible combination of two dices, each has the probability of 1 / 36
+        self.prob = 1 / 36
+
         for i in range(1, 7):
             for j in range(1, 7):
                 self.dice.append((i, j))
+
 
     # returns a string representing a unique nick name for your agent
     def nickname(self):
         # TODO: return a unique nick name for your agent
         return "yinuox, xuanry"
+
 
     # Given a ply, it sets a maximum for how far an agent
     # should go down in the search tree. If maxply==-1,
@@ -30,11 +34,13 @@ class BackgammonPlayer:
         if maxply != -1:
             self.maxply = maxply
 
+
     # If not None, it update the internal static evaluation
     # function to be func
     def useSpecialStaticEval(self, func=None):
         if func is not None:
             self.staticEval = func
+
 
     def initialize_move_gen_for_state(self, state, who, die1, die2):
         return self.GenMoveInstance.gen_moves(state, who, die1, die2)
@@ -42,8 +48,9 @@ class BackgammonPlayer:
     # Given a state and a roll of dice, it returns the best move for
     # the state.whose_move
 
+
     def move(self, state, die1, die2):
-        self.dict = {}                  # reset the dictionary for every move
+        self.dict.clear()                 # reset the dictionary for every move
         value = self.expectiminimax(state, state.whose_move, self.maxply, die1, die2)
         return self.dict[value]
 
@@ -51,126 +58,79 @@ class BackgammonPlayer:
     # for the two players (W and R) -- more positive numbers are good for W
     # while more negative numbers are good for R
 
-    def staticEval(self, state):
-        red = 0
-        white = 0
 
+    def staticEval(self, state):
+        ''' Count the pips counts for white and red player. 
+        Fewer pips counts means better
+        '''
+        red = 0                    # pips counts 
+        white = 0
+        
         for i in range(24):
             for checker in state.pointLists[i]:
-                if(checker == 0):
-                    white += (24 - i)
+                if(checker == 0):    
+                    white += (24 - i)           
                 else:
                     red += (i + 1)
-
+            
         for checker in state.bar:
             if(checker == 0):
                 white = white + 25
             else:
                 red = red + 25
-
+        
         red -= 25 * len(state.red_off)
         white -= 25 * len(state.white_off)
+
+        # return the difference
+        # more positive means good for white (white gets closer to win)
+        # negative means good for red (red gets closer to win)
         return red - white
 
+
     def expectiminimax(self, state, whoseMove, plyLeft, die1, die2):
         if plyLeft == 0:
             return self.staticEval(state)
 
-        if whoseMove == 0:            # maximizing agent
+        if whoseMove == 0:            
             bestVal = self.MIN
-            bestMove = None
-            move_state_list = self.get_all_possible_moves_state(state, die1, die2)
-            for (m, s) in move_state_list:
-                val = 0
-                for (dice1, dice2) in self.dice:
-                    newVal = self.expectiminimax(s, s.whose_move, plyLeft - 1, dice1, dice2)
-                    val += newVal / 36
-                    
-                if val > bestVal:
-                    bestVal = val
-                    bestMove = m
-
-            self.dict[bestVal] = bestMove
-            return bestVal
-            
         else:
             bestVal = self.MAX
-            bestMove = None
-            move_state_list = self.get_all_possible_moves_state(state, die1, die2)
-            for (m, s) in move_state_list:
-                val = 0
-                for (dice1, dice2) in self.dice:
-                    newVal = self.expectiminimax(s, s.whose_move, plyLeft - 1, dice1, dice2)
-                    val += 1/36 * newVal
-                    
-                if val < bestVal:   
-                    bestVal = val
-                    bestMove = m
 
-            self.dict[bestVal] = bestMove
-            return bestVal
+        bestMove = None
+        move_state_list = self.get_all_possible_moves_state(state, die1, die2)
+        for (m, s) in move_state_list:
+            newVal = 0   # expected value 
 
-    '''
-    def expectiminimax(self, state, whoseMove, plyLeft, die1, die2):
+            # environment
+            for (dice1, dice2) in self.dice:
+                newVal += self.prob * self.expectiminimax(s, s.whose_move, plyLeft - 1, dice1, dice2)
+                
+            if (whoseMove == 0 and newVal > bestVal) or \
+                (whoseMove == 1 and newVal < bestVal):                  
+                bestVal = newVal
+                bestMove = m 
 
-        if plyLeft == 0:
-            return self.staticEval(state)
+        self.dict[bestVal] = bestMove
+        return bestVal
 
-        if whoseMove == 0:            # maximizing agent
-            bestVal = self.MIN
-            bestMove = None
-            for (die1, die2) in self.dice:
-                # move_state_list = self.get_all_possible_moves_state(state, die1, die2)
-                # for (m, s) in move_state_list:
-                newVal = self.expectiminimax(state, -1, plyLeft - 1, die1, die2)
-                if bestVal > newVal:
-                    bestVal = newVal
-                    bestMove = m  # determine the best move here
-            self.dict[bestVal] = bestMove
-            return bestVal
-            # // Return value of maximum-valued child node
-            # let α := -∞
-            # foreach child of node
-            # α := max(α, expectiminimax(child, depth-1))
-        elif whoseMove == 1:         # mininizing agent
-            bestVal = self.MAX
-            bestMove = None
-            for (die1, die2) in self.dice:
-                # move_state_list = self.get_all_possible_moves_state(state, die1, die2)
-                # for (m, s) in move_state_list:
-                newVal = self.expectiminimax(state, -1, plyLeft - 1, die1, die2)
-                if bestVal < newVal:
-                    bestVal = newVal
-                    bestMove = m
-            self.dict[bestVal] = bestMove
-            return bestVal
-        else:                           # random agent (圆圈)
-            # Return weighted average of all child nodes' values
-            bestVal = 0
-            move_state_list = self.get_all_possible_moves_state(state, die1, die2)
-            for (m, s) in move_state_list:
-                bestVal += (1/36) * self.expectiminimax(s, s.whose_move, plyLeft - 1, die1, die2)
-            # pick a move randomly? will never pick
-            return bestVal
-    '''
 
     def get_all_possible_moves_state(self, state, die1, die2):
         """Uses the mover to generate all legal (moves, state). Returns an array of tuples"""
-        move_generator = self.initialize_move_gen_for_state(
-            state, state.whose_move, die1, die2)
+        # reset the move_generator to every new state passed in
+        move_generator = self.initialize_move_gen_for_state(state, state.whose_move, die1, die2)
         move_state_list = []
         done_finding_moves = False
         any_non_pass_moves = False
         while not done_finding_moves:
             try:
-                m = next(move_generator)         # Gets a (move, state) pair.
-                # print("next returns: ", m[0], 'who: ', state.whose_move, 'plyLeft: ', plyLeft)          # Prints out the move.    For debugging.
-                if m[0] != 'p':
-                    any_non_pass_moves = True
-                    # Add the (move, state) to the list.
-                    move_state_list.append(m)
+                m = next(move_generator)         # Gets a (move, state) pair.        
+                if m[0] != 'p':                          
+                    any_non_pass_moves = True          
+                    move_state_list.append(m)    # Add the (move, state) to the list.
             except StopIteration as e:
                 done_finding_moves = True
-        if not any_non_pass_moves:
-            move_state_list.append(('p', state))
+        if not any_non_pass_moves:                  # if no move, then the state doesn't change
+            move_state_list.append(('p', state))    
         return move_state_list
+
